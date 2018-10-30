73d079966ca89194fb4431221d45935f599f7cd1
Vijay G <vjremo@gmail.com>
2018-10-30 09:42:17 -0400

    feat: add contacts and render to DOM

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+     return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ContactList contacts={[
+        { name: 'Michael' },
+        { name: 'Ryan' },
+        { name: 'Tyler' }] }/>
+        <ContactList contacts={[
+      { name: 'Amanda' },
+      { name: 'Claire' },
+      { name: 'Lilly' }]}/>
       </div>
     );
   }
